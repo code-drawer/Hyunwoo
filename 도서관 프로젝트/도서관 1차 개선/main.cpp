1009b4a4a20e683f6fcd13f3005f2fdb8b62d605
현우 김 <anycatim3@hanyang.ac.kr>
2022-02-05 14:58:46 +0900

    booklist, userlist 배열 -> vector로 변경

---
+++ 도서관 프로젝트/도서관 1차 개선/main.cpp
@@ -0,0 +1,104 @@
+#include "도서관.h"
+
+Book book1{ "어린왕자","생텍쥐페리","동화",1000 };
+Book book2{ "정의란 무엇인가","마이클 샌델","인문학",2000 };
+Book book3{ "십자군 이야기","시오노 나나미","인문학",2001 };
+Book book4{ "일반물리학","데이비드 홀리데이","과학/공학",3000 };
+Book book5{ "신데렐라","샤를 페로","동화",1001 };
+Book book6{ "코스모스","칼 세이건","과학/공학",3001 };
+Book book7{ "메타버스","김상균","과학/공학",3002 };
+Book book8{ "종의 기원","찰스 로버트 다윈","과학/공학",3003 };
+Book book9{ "오디세이아","호메로스","소설",4000 };
+
+User user1{ "김현우","01038161033",100 };
+User user2{ "정연한","01030414307",101 };
+User user3{ "박도현","01039041238",102 };
+User user4{ "김도영","01079416794",103 };
+User user5{ "안수환","01059821975",104 };
+
+int main() {
+	도서관 중앙도서관{}; // 중앙도서관 객체 생성
+
+	중앙도서관.addbook(book1); // 중앙도서관에 book1 추가
+	중앙도서관.addbook(book2); // 중앙도서관에 book2 추가
+	중앙도서관.addbook(book3); // 중앙도서관에 book3 추가
+	중앙도서관.addbook(book4); // 중앙도서관에 book4 추가
+	중앙도서관.addbook(book5); // 중앙도서관에 book5 추가
+
+	중앙도서관.adduser(user1);
+	중앙도서관.adduser(user2);
+	중앙도서관.adduser(user3);
+	중앙도서관.adduser(user4);
+	중앙도서관.adduser(user5);
+
+	while (true) {
+		cout << "중앙도서관 관리 시스템" << endl << endl;
+		cout << "1. 도서목록 확인" << endl << "2. 도서대출" << endl << "3. 도서반납" << endl <<
+			"4. 도서추가" << endl << "5. 도서삭제" << endl << "6. 유저목록 확인" << endl << 
+			"7. 유저추가" << endl << "8. 유저삭제" << endl << "9. 프로그램 종료" << endl << endl;
+
+		string bookname;
+		string username;
+		string phonenum;
+		int usernum;
+		string author;
+		string genre;
+		int booknum;
+
+		int choice(0);
+		cin >> choice;
+		switch (choice) {
+		case 1: // 도서목록 확인
+			중앙도서관.displayBook();
+			cout << endl;
+			break;
+		case 2: // 도서대출
+			cout << "유저이름과 책이름을 순서대로 입력해주세요" << endl;
+			cin >> username >> bookname;
+			중앙도서관.borrow(username, bookname);
+			cout << endl;
+			break;
+		case 3: // 도서반납
+			cout << "유저이름과 책이름을 순서대로 입력해주세요" << endl;
+			cin >> username >> bookname;
+			중앙도서관.returnbook(username, bookname);
+			cout << endl;
+			break;
+		case 4: { //도서추가
+			cout << "책이름과 저자, 장르,책번호를 순서대로 입력해주세요" << endl;
+			cin >> bookname >> author >> genre >> booknum;
+			Book tmp{ bookname, author, genre, booknum };
+			중앙도서관.addbook(tmp);
+			cout << endl;
+			break; }
+		case 5: { //도서삭제
+			cout << "삭제할 책이름을 입력하세요" << endl;
+			cin >> bookname;
+			중앙도서관.removebook(bookname);
+			cout << endl;
+			break; }
+		case 6: //유저목록 확인
+			중앙도서관.displayuser();
+			cout << endl;
+			break;
+		case 7: { //유저추가 
+			cout << "유저이름과 핸드폰번호, 유저번호를 순서대로 입력해주세요" << endl;
+			cin >> username >> phonenum >> usernum;
+			User tmp{ username, phonenum, usernum };
+			중앙도서관.adduser(tmp);
+			cout << endl;
+			break; }
+		case 8: { //유저삭제
+			cout << "삭제할 유저이름을 입력하세요" << endl;
+			cin >> username;
+			중앙도서관.removeuser(username);
+			cout << endl;
+			break; }
+		default: return 0;
+		}
+
+		system("pause");
+		system("cls");
+	}
+	return 0;
+}
---
+++ 도서관 프로젝트/도서관 1차 개선/도서관.h
@@ -0,0 +1,125 @@
+#pragma once
+#include "Book.h"
+#include "User.h"
+#include <vector> //vector: 가변길이 배열
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+using std::vector;
+
+class 도서관 {
+public:
+	도서관()
+		: booklist{ }, userlist{ } {
+	}
+	도서관(Book booklist[20], User userlist[10] = nullptr) {
+		
+	}
+
+	// 책 추가
+	void addbook(Book 넣을책) {
+		booklist.resize(booklist.size() + 1); // booklist의 크기를 1 증가시켜준다.
+		// 범위 기반 반복문
+		// auto: 초기화 할 값에 따라 자료형을 자동으로 추론하는 키워드
+		for (auto& elem : booklist) {
+			if (elem.get_booknum() == 0) {
+				elem = 넣을책;
+				break;
+			}
+		}
+	}
+
+	// 책 삭제
+	void removebook(string bookname) {
+		for (auto& elem : booklist) {
+			if (elem.get_bookname() == bookname) {
+				elem.remove();
+			}
+		}
+	}
+
+	// 들어있는 책들의 이름 출력
+	void displayBook() {
+		for (auto& elem : booklist) {
+			if (elem.get_booknum() != 0) {
+				cout << elem.get_bookname() << endl;
+			}
+		}
+	}
+
+	//유저 추가
+	void adduser(User 추가할유저) {
+		userlist.resize(userlist.size() + 1);
+
+		for (auto& elem : userlist) {
+			if (elem.get_usernum() == 0) {
+				elem = 추가할유저;
+				break;
+			}
+		}
+	}
+
+	//유저 삭제
+	void removeuser(string username) {
+		for (auto& elem : userlist) {
+			if (elem.get_username() == username) {
+				elem.remove();
+			}
+		}
+	}
+
+	//유저목록 출력
+	void displayuser() {
+		for (auto& elem : userlist) {
+			if (elem.get_usernum() != 0) {
+				cout << elem.get_username() << endl;
+			}
+		}
+	}
+
+	void borrow(string username, string bookname) {
+		for (auto& userElem : userlist) {
+			if (userElem.get_username() == username) {
+				for (auto& bookElem : booklist) {
+					if (bookElem.get_bookname() == bookname && userElem.get_borrowstat() == false) {
+						userElem.set_booknum(bookElem.get_booknum());
+						userElem.set_borrowstat();
+						cout << "선택한 책이 정상적으로 대출되었습니다." << endl;
+						return;
+					}
+				}
+				cout << "입력한 책이 도서관에 없습니다." << endl;
+				return;
+			}
+		}
+		cout << "유저의 이름이 같지 않습니다." << endl;
+		return;
+	}
+	
+	void returnbook(string username, string bookname) {
+		for (auto& userElem : userlist) {
+			if (userElem.get_username() == username) {
+				for (auto& bookElem : booklist) {
+					if (bookElem.get_bookname() == bookname && userElem.get_borrowstat() == true) {
+						userElem.set_booknum(-1);
+						userElem.set_borrowstat();
+						cout << "선택한 책이 정상적으로 반납되었습니다." << endl;
+						return;
+					}
+				}
+				cout << "해당 책을 대출하지 않았습니다." << endl;
+				return;
+			}
+		}
+		cout << "유저의 이름이 같지 않습니다." << endl;
+		return;
+	}
+
+private:
+	vector<Book> booklist;
+
+	vector<User> userlist;
+};
